618a42e034b7476f74d08f60b19c5204e71e090e
Nikos Nikolaidis <nikos.gate4.nik@gmail.com>
2025-05-28 19:20:15 +0300

    Add env value in test, and a mock JavaMailSender

--- src/test/java/gr/uom/user_management/UserManagementApplicationTests.java
+++ src/test/java/gr/uom/user_management/UserManagementApplicationTests.java
@@ -2,10 +2,12 @@ package gr.uom.user_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestMailConfig.class)
 class UserManagementApplicationTests {
 
 	@Test
