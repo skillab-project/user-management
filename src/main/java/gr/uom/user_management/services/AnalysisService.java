b91fade533ab30759888189a1d194fbc50d6973e
Nikos Nikolaidis <nikos.gate4.nik@gmail.com>
2025-08-06 16:03:46 +0300

    Add Transactional to checkIfSameAnalysisExists

--- src/main/java/gr/uom/user_management/services/AnalysisService.java
+++ src/main/java/gr/uom/user_management/services/AnalysisService.java
@@ -7,6 +7,7 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -19,6 +20,7 @@ public class AnalysisService {
     @Autowired
     AnalysisAsyncService analysisAsyncService;
 
+    @Transactional()
     public Analysis checkIfSameAnalysisExists(String sessionId, String filterOccupation, String filterMinDate, String filterMaxDate, String filterSources, Integer limitData) {
         Optional<Analysis> existing = analysisRepository
                 .findBySessionIdAndFilterOccupationAndFilterMinDateAndFilterMaxDateAndFilterSourcesAndLimitData(
